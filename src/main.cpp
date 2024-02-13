c14a7646855889e09b3bf41fb38efe10d707d8de
kukuikar <denis@perepetsky.ru>
2024-02-13 17:19:17 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 #include <WiFiUdp.h>
 #include <WiFi.h>
 // #include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
-#include <EEPROM.h>
+//#include <ESPAsyncWebServer.h>
+//#include <AsyncElegantOTA.h>
+//#include <EEPROM.h>
 #include <FastBot.h>
 
-AsyncWebServer server(80);
+//AsyncWebServer server(80);
 
 // #define MKD_Guest 123
 #ifdef MKD_Guest
@@ -60,38 +60,36 @@ void setup()
   Serial.println(WiFi.localIP());
   udp.begin(port);
 
-  MOT_CR1_Winch.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR1_Winch.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR1_Winch.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR1_Winch.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR1_Winch.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  MOT_CR2_Winch.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR2_Winch.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR2_Winch.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR2_Winch.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR2_Winch.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  MOT_CR1_Arm.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR1_Arm.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR1_Arm.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR1_Arm.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR1_Arm.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  MOT_CR2_Arm.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR2_Arm.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR2_Arm.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR2_Arm.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR2_Arm.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  MOT_CR1_Rot.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR1_Rot.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR1_Rot.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR1_Rot.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR1_Rot.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  MOT_CR2_Rot.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
+  // MOT_CR2_Rot.setMinDuty(20); // мин. ШИМ  ////////////////////////////////////////////////
   // MOT_CR2_Rot.reverse(1);      // реверс  ////////////////////////////////////////////////
-  MOT_CR2_Rot.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
+  // MOT_CR2_Rot.setDeadtime(1); // deadtime  ////////////////////////////////////////////////
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", hostname); });
+  //server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(200, "text/plain", hostname); });
 
-  AsyncElegantOTA.begin(&server); // Start AsyncElegantOTA
-  server.begin();
+  //AsyncElegantOTA.begin(&server); // Start AsyncElegantOTA
+  //server.begin();
 
   bot.attach(newMsg);
-  // bot.showMenu("STATUS");
   bot.sendMessage(IpAddress2String(WiFi.localIP()), DPE_CHAT_ID);
 }
 
@@ -126,7 +124,7 @@ void loop()
       GParser data(buf);
       int ints[data.amount()];
       data.parseInts(ints);
-
+      int en = ints[1];
       Serial.println(buf);
 
       if (newIP.fromString(buf))
@@ -138,72 +136,53 @@ void loop()
           Serial.print(" -- >> ");
           ServIP = newIP;
         }
-        Serial.println(ServIP);
       }
       else if (ints[0] == 2)
       {
-        if (ints[1] == 1)
+        int rot_val = map(ints[4], 0, 255, -255, 255);
+        int win_val = map(ints[6], 0, 255, -255, 255);
+        int arm_val = map(ints[5], 0, 255, -255, 255);
+
+        rot_val = en * abs(rot_val) <= 2 ? 0 : rot_val;
+        win_val = en * abs(win_val) <= 2 ? 0 : win_val;
+        arm_val = en * abs(arm_val) <= 2 ? 0 : arm_val;
+
+        Serial.printf("INTS %d\t%d\t%d\n", ints[4], ints[6], ints[5]);
+        Serial.printf("VALS %d\t%d\t%d\n", rot_val, win_val, arm_val);
+        // rot_val = 0;
+        // win_val = 0;
+        // arm_val = 0;
+
+        switch (ints[3])
         {
-          int rot_val = map(ints[4], 0, 255, -255, 255);
-          int win_val = map(ints[6], 0, 255, -255, 255);
-          int arm_val = map(ints[5], 0, 255, -255, 255);
+        case 0: // single mode Crane 1
+          Serial.println("Single mode 1");
+          MOT_CR1_Rot.setSpeed(rot_val);
+          MOT_CR1_Winch.setSpeed(win_val);
+          MOT_CR1_Arm.setSpeed(arm_val);
+          break;
 
-          rot_val = abs(rot_val) <= 2 ? 0 : rot_val;
-          win_val = abs(win_val) <= 2 ? 0 : win_val;
-          arm_val = abs(arm_val) <= 2 ? 0 : arm_val;
+        case 1: // single mode Crane 2
+          Serial.println("Single mode 2");
+          MOT_CR2_Rot.setSpeed(rot_val);
+          MOT_CR2_Winch.setSpeed(win_val);
+          MOT_CR2_Arm.setSpeed(arm_val);
+          break;
 
-          Serial.printf("%d\t%d\t%d\n", rot_val, win_val, arm_val);
-          // rot_val = 0;
-          // win_val = 0;
-          // arm_val = 0;
+        case 2: // mirror mode
+          Serial.println("Mirror Mode");
+          MOT_CR1_Rot.setSpeed(-1 * rot_val);
+          MOT_CR1_Winch.setSpeed(win_val);
+          MOT_CR1_Arm.setSpeed(arm_val);
 
-          switch (ints[3])
-          {
-          case 0: // single mode Crane 1
-            Serial.print("1st crane\t");
-            Serial.printf("%d\t%d\t%d\n", rot_val, win_val, arm_val);
-            MOT_CR1_Rot.setSpeed(rot_val);
-            MOT_CR1_Winch.setSpeed(win_val);
-            MOT_CR1_Arm.setSpeed(arm_val);
-            break;
-
-          case 1: // single mode Crane 2
-                  /*
-                    Serial.print("2nd crane\t");
-                    Serial.printf("%d\t%d\t%d\n", rot_val, win_val, arm_val);
-                    MOT_CR2_Rot.setSpeed(rot_val);
-                    MOT_CR2_Winch.setSpeed(win_val);
-                    MOT_CR2_Arm.setSpeed(arm_val);
-                    */
-            break;
-
-          case 2: // mirror mode
-            Serial.println("Mirror Mode");
-            /*
-            MOT_CR1_Rot.setSpeed(-1 * rot_val);
-            MOT_CR1_Winch.setSpeed(win_val);
-            MOT_CR1_Arm.setSpeed(arm_val);
-
-            MOT_CR2_Rot.setSpeed(rot_val);
-            MOT_CR2_Winch.setSpeed(win_val);
-            MOT_CR2_Arm.setSpeed(arm_val);
-            */
-
-            break;
-          }
-        }
-        else
-        {
-          MOT_CR1_Rot.setSpeed(0);
-          MOT_CR1_Winch.setSpeed(0);
-          MOT_CR1_Arm.setSpeed(0);
+          MOT_CR2_Rot.setSpeed(rot_val);
+          MOT_CR2_Winch.setSpeed(win_val);
+          MOT_CR2_Arm.setSpeed(arm_val);
+          break;
         }
       }
     }
   }
-
-  Serial.printf("State\n\t\tRot1\t%d Arm1\t%d Win1\t%d\n", MOT_CR1_Rot.getState(), MOT_CR1_Winch.getState(), MOT_CR1_Arm.getState());
-  Serial.printf("Speed\n\t\tRot1\t%d Arm1\t%d Win1\t%d\n", MOT_CR1_Rot.getSpeed(), MOT_CR1_Winch.getSpeed(), MOT_CR1_Arm.getSpeed());
 
   static uint32_t tmr = millis();
   if (millis() - tmr > 25)
